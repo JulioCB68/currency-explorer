9bfdbc252155d23d561af6afdedbafa774c36b57
Júlio César <juliobento237@gmail.com>
2024-04-18 12:13:10 -0300

    chore: setup font and meta data

---
+++ src/config/meta-data.ts
@@ -0,0 +1,7 @@
+import { Metadata } from 'next'
+
+export const metaData: Metadata = {
+  title: 'CurrencyXplorer',
+  description:
+    'This modern web application aims to provide users with a seamless experience for exploring real-time currency exchange rates, performing currency conversions, and visualizing exchange rate trends over time.',
+}
